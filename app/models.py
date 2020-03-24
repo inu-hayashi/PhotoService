155c1b85a79fa339295dbe6321f777589f0ca85f
takomisonikomi@gmail.com <takomisonikomi@gmail.com>
2020-03-24 11:04:47 +0900

    トップに画像表示

--- app/models.py
+++ app/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Photo(models.Model):
     title = models.CharField(max_length=150)
     comment = models.TextField(blank=True)
     image = models.ImageField(upload_to='photos')
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
