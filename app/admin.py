3d3618fecd1cb139a0c7d2fed2229b6a5b437dbd
takomisonikomi@gmail.com <takomisonikomi@gmail.com>
2020-03-26 11:55:29 +0900

    ログイン機能

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import Photo
+from .models import Category, Photo
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_link = ('id', 'title')
+
 
 class PhotoAdmin(admin.ModelAdmin):
     list_display = ('id', 'title')
     list_display_link = ('id', 'title')
 
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Photo, PhotoAdmin)
--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.models import User
 from .models import Photo
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 
 def index(request):
     photos = Photo.objects.all().order_by('-created_at')
@@ -8,6 +10,22 @@ def index(request):
 
 def users_detail(request, pk):
     user = get_object_or_404(User, pk=pk)
+    photos = user.photo_set.all().order_by('-created_at')
     return render(request, 'app/users_detail.html',{'user':user})
+
+def signup(request):
+    if request.method== 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            input_username = form.cleaned_data['username']
+            input_password = form.cleaned_data['password1']
+            new_user = authenticate(username = input_username,password = input_password)
+            if new_user is not None:
+                login(request, new_user)
+                return redirect('app:users_detail',pk=new_user.pk)
+    else:
+        form = UserCreationForm()
+    return render(request, 'app/signup.html', {'form':form})
 
 
